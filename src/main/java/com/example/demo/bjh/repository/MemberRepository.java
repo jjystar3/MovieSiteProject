d70303784cf39bec9e915eb0163fcba8fa419b6a
JuYoung <jjystar3@gmail.com>
2024-10-25 16:09:55 +0900

    영상 하나씩 추출하기

--- src/main/java/com/example/demo/bjh/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.bjh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.bjh.entity.MemberEntity;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-}
